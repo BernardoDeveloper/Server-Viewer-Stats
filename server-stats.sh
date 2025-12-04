aee18731c8f4de1c8181e9435e55e4299f028452
BernardoDeveloper <bernardogualberto8@gmail.com>
2025-12-04 09:25:40 -0300

    feat: add ram usage

--- server-stats.sh
+++ server-stats.sh
@@ -3,21 +3,39 @@
 # function to read the CPU usage
 get_cpu_usage() {
 	cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8 "%"}')
-	echo -ne "\rUso de CPU: $cpu_usage   "
+	echo "CPU: $cpu_usage   "
 }
+
+# function to read the RAM usage
+get_ram_usage() {
+	mem_info=$(free -h)
+
+	used_ram=$(echo "$mem_info" | awk 'NR==2{print $3}')
+	total_ram=$(echo "$mem_info" | awk 'NR==2{print $2}')
+
+  used_ram_percentage=$(free -m | awk '/^Mem:/ {printf "%.2f%\n", $3/$2*100}')
+
+	echo "Used RAM: $used_ram - $used_ram_percentage"
+	echo "Total RAM: $total_ram"
+}
+
+# function to get the disk usage
+#get_disk_usage() {}
 
 # main funcion
 main() {
 	get_cpu_usage
+	get_ram_usage
 }
 
 # verify the flag
 if [[ "$1" == "--real-time" ]]; then
-	echo "Monitorando CPU em tempo real... (CTRL+C para parar)"
-
 	while true; do
+	  echo "Showing stats in real time... (CTRL+C to stop)"
 		main
-		sleep 1
+
+		sleep 2
+    clear
 	done
 else
 	main
