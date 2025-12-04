883580432fdfdd29b4fede4481548fc781f7c712
BernardoDeveloper <bernardogualberto8@gmail.com>
2025-12-04 18:30:48 -0300

    feat: disk usage and top 5 process

--- server-stats.sh
+++ server-stats.sh
@@ -4,6 +4,7 @@
 get_cpu_usage() {
 	cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8 "%"}')
 	echo "CPU: $cpu_usage   "
+  echo
 }
 
 # function to read the RAM usage
@@ -17,15 +18,51 @@ get_ram_usage() {
 
 	echo "Used RAM: $used_ram - $used_ram_percentage"
 	echo "Total RAM: $total_ram"
+
+  echo
 }
 
 # function to get the disk usage
-#get_disk_usage() {}
+get_disk_usage() {
+  used_disk_percentage=$(df -h 2>/dev/null | grep drivers | awk '{print $5}') # 2>/dev/null -> ignore errors and only show the normal output
+  echo "Used DISK main driver: $used_disk_percentage" 
+
+  used_disk_memory=$(df -h 2>/dev/null | grep drivers | awk '{print $3}')
+  echo "Total DISK: $used_disk_memory"
+
+  echo
+}
+
+# get top 5 process
+# parameter type: cpu || ram
+get_top_proccess() {
+  local type=$1
+
+  if [[ "$type" == "cpu" ]]; then
+    echo -e "TOP 5 CPU process\n"
+    
+    ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 6
+  elif [[ "$type" == "ram" ]]; then
+    echo -e "\n\nTOP 5 RAM process\n"
+
+    ps aux --sort -%mem | head -n 6
+  else
+    echo "[error] in function get_top_proccess need use as parameter ram or cpu"
+  fi
+}
 
 # main funcion
 main() {
 	get_cpu_usage
 	get_ram_usage
+  get_disk_usage
+
+  echo -e "\n==============================================================\n"
+  echo -e "\t\t\tTOP 5 PROCESS"
+  echo -e "\n==============================================================\n"
+
+  get_top_proccess cpu
+  get_top_proccess ram
 }
 
 # verify the flag
